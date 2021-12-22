c8037ac63ea5c640e4ace2fffaf7c001275beceb
Amaar Shaher <amaar.shaher.ruh.k@tuwaiq.edu.sa>
2021-12-22 18:27:56 +0300

    Log_in

---
+++ app/src/main/java/com/example/eilaji/LogInFragment.kt
@@ -0,0 +1,112 @@
+package com.example.eilaji
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class LogInFragment : Fragment() {
+    var isSignedIn = false
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    val providers = arrayListOf(
+      //  AuthUI.IdpConfig.PhoneBuilder().build()
+           AuthUI.IdpConfig.EmailBuilder().build(),
+       // AuthUI.IdpConfig.GoogleBuilder().build()
+    )
+
+    val signInIntent = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.log_in -> {
+                signInLauncher.launch(signInIntent)
+            }
+            R.id.log_out -> {
+                signOut()
+                isSignedIn = false
+            }
+
+        }
+
+        return true
+    }
+
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+
+        if (isSignedIn) {
+            menu.findItem(R.id.log_in)?.isVisible = false
+            menu.findItem(R.id.log_out)?.isVisible = true
+        } else {
+            menu.findItem(R.id.log_in)?.isVisible =  true
+            menu.findItem(R.id.log_out)?.isVisible = false
+        }
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = Firebase.auth.currentUser
+        if (currentUser != null) {
+            isSignedIn = true
+        }
+
+    }
+
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+            println(user?.email)
+        } else {
+            println("else")
+        }
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this.requireContext())
+
+    }
+}
