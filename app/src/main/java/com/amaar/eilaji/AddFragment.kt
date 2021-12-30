0a47ed0ad57ff69f5b3114774951b1e1716b5c61
Amaar Shaher <amaar.shaher.ruh.k@tuwaiq.edu.sa>
2021-12-30 16:41:27 +0300

    creat add fragment with view model

--- app/src/main/java/com/amaar/eilaji/AddFragment.kt
+++ app/src/main/java/com/amaar/eilaji/AddFragment.kt
@@ -4,43 +4,43 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
+
 import android.view.ViewGroup
-import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
-import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+
 import androidx.fragment.app.viewModels
+
 import androidx.navigation.Navigation
 import com.amaar.eilaji.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
 
-//private lateinit var binding: FragmentAddBinding
-//
-//
-//
-//
-//  private val viewModel : Viewmodel by viewModels()
-//
-//
-//  override fun onCreateView(
-//      inflater: LayoutInflater,
-//      container: ViewGroup?,
-//      savedInstanceState: Bundle?
-//  ): View? {
-//      binding = FragmentAddBinding.inflate(inflater, container, false)
-//      return binding.root
-//  }
-//
-//
-//  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//      binding.apply {
-//          lifecycleOwner=viewLifecycleOwner
-//          viewmodelvar=viewModel
-//      }
-//      binding?.saveBotton?.setOnClickListener { view:View ->
-//          Navigation.findNavController(view).navigate(AddFragmentDirections.actionAddFragmentToTodoFragment())
-//          viewModel.AddToList()
-//      }
-//  }
+  private lateinit var binding: FragmentAddBinding
+
+
+
+
+    private val viewModel : MyViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+           // lifecycleOwner=viewLifecycleOwner
+            viewmodelvar=viewModel
+        }
+        binding?.saveBtnView?.setOnClickListener { view:View ->
+            Navigation.findNavController(view).navigate(AddFragmentDirections.actionAddFragmentToHomePageFragment())
+            viewModel.AddToList()
+        }
+    }
 }
